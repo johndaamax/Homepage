c0802453d61803ca00acf672d87557f4ccf601c0
johndaamax <ia.uoistud@gmail.com>
2019-02-28 01:13:24 +0200

    Final edits before deploy

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-	<BrowserRouter>
+	<BrowserRouter basename='/homepage'>
 		<App />
 	</BrowserRouter>,
 	document.getElementById('root')
