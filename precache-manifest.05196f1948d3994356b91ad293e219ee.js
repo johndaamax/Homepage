0ab934aa30f01db87ed2815a2f042fb0f5a98c6f
johndaamax <ia.uoistud@gmail.com>
2019-02-28 16:33:17 +0200

    Updates

---
+++ precache-manifest.05196f1948d3994356b91ad293e219ee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc200705200bac3d5d44",
+    "url": "/homepage/static/css/main.de5a9a9f.chunk.css"
+  },
+  {
+    "revision": "dc200705200bac3d5d44",
+    "url": "/homepage/static/js/main.dc200705.chunk.js"
+  },
+  {
+    "revision": "810d44d7fb3dbe398ef1",
+    "url": "/homepage/static/js/runtime~main.810d44d7.js"
+  },
+  {
+    "revision": "b1560f634fd2978682e7",
+    "url": "/homepage/static/js/2.b1560f63.chunk.js"
+  },
+  {
+    "revision": "544deee380000609f7a26fa419190f4b",
+    "url": "/homepage/static/media/portrait.544deee3.jpg"
+  },
+  {
+    "revision": "0fd4b5ff312028782bf7bd82da5890ea",
+    "url": "/homepage/static/media/discord_bot.0fd4b5ff.jpg"
+  },
+  {
+    "revision": "423f48e07dd46f461d948721ffc28c64",
+    "url": "/homepage/static/media/rgbgame.423f48e0.png"
+  },
+  {
+    "revision": "7b6d899cf9c0f6900cbc770da87b9ce4",
+    "url": "/homepage/static/media/refactoring.7b6d899c.png"
+  },
+  {
+    "revision": "7f45c741d73b79df756bcd415ed1e790",
+    "url": "/homepage/static/media/background.7f45c741.jpg"
+  },
+  {
+    "revision": "39f2a7b5be79677cd6a2a4799e5fbec3",
+    "url": "/homepage/index.html"
+  }
+];
